63087cc4b52089f67369451aa7fba52e2ea295b0
chenmengdi <1599126082@qq.com>
2018-01-26 09:42:32 +0800

    Initial Commit

---
+++ 贝塞尔曲线绘制电池/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  贝塞尔曲线绘制电池
+//
+//  Created by 陈孟迪 on 2018/1/26.
+//  Copyright © 2018年 陈孟迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 贝塞尔曲线绘制电池/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  贝塞尔曲线绘制电池
+//
+//  Created by 陈孟迪 on 2018/1/26.
+//  Copyright © 2018年 陈孟迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
